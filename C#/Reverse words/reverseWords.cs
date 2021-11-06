1d70ab33e8c8bd7f1faebd7b3c2e6704480d0c41
AllanDwyre <allan.dwyre@gmail.com>
2021-11-06 19:29:50 +0100

    main

--- C#/Reverse words/reverseWords.cs
+++ C#/Reverse words/reverseWords.cs
@@ -19,3 +19,16 @@ public static class Kata
         return result;
     }
 }
+
+// ----------------------- Best Solution :  --------------------
+
+// using System;
+// using System.Linq;
+
+// public static class Kata
+// {
+//   public static string ReverseWords(string str)
+//   {
+//     return string.Join(" ", str.Split(' ').Select(i => new string(i.Reverse().ToArray())));
+//   }
+// }
