f01df4f6a99e653a9eb87dffdd239f18da294856
AllanDwyre <allan.dwyre@gmail.com>
2021-11-09 11:45:19 +0100

    main

--- C#/MorceCodeDecode/MorceCodeDecode.cs
+++ C#/MorceCodeDecode/MorceCodeDecode.cs
@@ -8,9 +8,14 @@ class MorseCodeDecoder
         string[] words = morseCode.Split("   "); // .. -. -.
         for (int i = 0; i < words.Length; i++)
         {
+            if(words[i] == String.Empty){
+                continue;
+            }
+
             string[] letters = words[i].Split(' ');// ..
             for (int y = 0; y < letters.Length; y++)
             {
+                Console.WriteLine(letters[y]);
                 result += MorseCode.Get(letters[y]);
             }
             if (i < words.Length - 1)
@@ -21,3 +26,19 @@ class MorseCodeDecoder
         return result;
     }
 }
+
+// ------------------------- BEST SOLUTION -----------------------
+// using System;
+// using System.Linq;
+
+// class MorseCodeDecoder
+// {
+//   public static string Decode(string morseCode)
+//   {
+//     var words = morseCode.Trim().Split(new[] {"   "}, StringSplitOptions.None);
+//     var translatedWords = words.Select(word => word.Split(' ')).Select(letters => string.Join("", letters.Select(MorseCode.Get))).ToList();
+//     return string.Join(" ", translatedWords);
+//   }
+// }
+
+// I need to improve my Linq knowledge.
